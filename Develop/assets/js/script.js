666e3182d4fc5b142d304561f98e6da75439fe57
Cardenas1850 <99810565+Cardenas1850@users.noreply.github.com>
2022-07-21 19:04:25 -0400

    fixed local storage issue, wasn't showing up due to placement issues

--- Develop/assets/js/script.js
+++ Develop/assets/js/script.js
@@ -4,26 +4,27 @@ $("#currentDay").html(todayDate);
 //wrtie code to have save button work
 $(document).ready(function () {
     $(".saveBtn").on("click", function () {
-        var text = $(this).siblings(".description").value();
+        var text = $(this).siblings(".description").val();
         var time = $(this).parent().attr("id");
         //write code to save things into local storage
 localStorage.setItem(time, text);
     })
-})
+
 
 
 // get items from local storeage
 
-$("hour8a .description").value(localStorage.getItem("hour8a"));
-$("hour9a .description").value(localStorage.getItem("hour9a"));
-$("hour10a .description").value(localStorage.getItem("hour10a"));
-$("hour11a .description").value(localStorage.getItem("hour11a"));
-$("hour12p .description").value(localStorage.getItem("hour12p"));
-$("hour1p .description").value(localStorage.getItem("hour1p"));
-$("hour2p .description").value(localStorage.getItem("hour2p"));
-$("hour3p .description").value(localStorage.getItem("hour3p"));
-$("hour4p .description").value(localStorage.getItem("hour4p"));
-$("hour5p .description").value(localStorage.getItem("hour5p"));
-$("hour6p .description").value(localStorage.getItem("hour6p"));
-$("hour7p .description").value(localStorage.getItem("hour7p"));
-$("hour8p .description").value(localStorage.getItem("hour8p"));
+$("hour8a .description").val(localStorage.getItem("hour8a"));
+$("hour9a .description").val(localStorage.getItem("hour9a"));
+$("hour10a .description").val(localStorage.getItem("hour10a"));
+$("hour11a .description").val(localStorage.getItem("hour11a"));
+$("hour12p .description").val(localStorage.getItem("hour12p"));
+$("hour1p .description").val(localStorage.getItem("hour1p"));
+$("hour2p .description").val(localStorage.getItem("hour2p"));
+$("hour3p .description").val(localStorage.getItem("hour3p"));
+$("hour4p .description").val(localStorage.getItem("hour4p"));
+$("hour5p .description").val(localStorage.getItem("hour5p"));
+$("hour6p .description").val(localStorage.getItem("hour6p"));
+$("hour7p .description").val(localStorage.getItem("hour7p"));
+$("hour8p .description").val(localStorage.getItem("hour8p"));
+})
