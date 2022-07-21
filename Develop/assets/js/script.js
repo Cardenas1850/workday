fcf0bfd181d108993a92ad349105a5c886fc196c
Cardenas1850 <99810565+Cardenas1850@users.noreply.github.com>
2022-07-21 18:47:52 -0400

    added script

---
+++ Develop/assets/js/script.js
@@ -0,0 +1,8 @@
+//wrtie code to display date
+var todayDate = moment().format('dddd, MMM do YYYY');
+$("#currentDay").html(todayDate);
+//wrtie code to have save button work
+
+//write code to save things into local storage
+
+//
